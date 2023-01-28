3051034fb780c93a3c7db5dab676dc7439688d36
Qunzhang1996 <112466735+Qunzhang1996@users.noreply.github.com>
2023-01-28 23:02:43 +0100

    Add files via upload
    updating plot

--- dp_iteration.m
+++ dp_iteration.m
@@ -1,12 +1,15 @@
 function [i,P_next] = dp_iteration(aim_error,init_error,P_last,A2,B2,Q,R)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
-i=1;
-while (init_error>=aim_error)
+i=0;
+while 1
     P_next=Q+A2'*P_last*A2-A2'*P_last*B2*inv(R+B2'*P_last*B2)*B2'*P_last*A2;
     init_error=norm(P_next-P_last);
     P_last=P_next;
     i=i+1;
+    if aim_error>init_error
+        break
+    end
 end
 fprintf("This is the result of dp_literation solution\n")
 fprintf("the  value of literation is %d",i);
--- xu_plot.m
+++ xu_plot.m
@@ -17,5 +17,5 @@ function [X_K,uk,X1_k,X2_k,X3_k,UK] = xu_plot(A2,B2,N_input,Q,R,Pf,X0,X1_steps,X
     X1_k=X_K(1,1:X1_steps);
     X2_k=X_K(2,1:X2_steps);
     X3_k=X_K(3,1:X3_steps);
-    UK=[0,uk(:,1:U_step)];
+    UK=uk(:,1:U_step);
 end
